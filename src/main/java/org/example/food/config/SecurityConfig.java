d9939fbc7731e8abb12569a44b960b78da12a7a4
Kwak Hoon <77324409+hooncdn@users.noreply.github.com>
2024-04-15 17:36:28 +0900

    스프링 시큐리티 기본설정

---
+++ src/main/java/org/example/food/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package org.example.food.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((auth) -> {
+                    auth.requestMatchers("/", " /register").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .build();
+    }
+}
